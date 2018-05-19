472d8ae807e8269b3c75b7719d4fbf45022cd034
Daniel Obermeier <mail@jilocasin.de>
2018-05-19 18:19:24 +0200

    Added solver worker and orchestrator for multi-threaded NN computing

---
+++ src/main/de/jilocasin/nearestneighbour/NNSolverWorker.java
@@ -0,0 +1,44 @@
+package de.jilocasin.nearestneighbour;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import de.jilocasin.kdtree.model.KdPoint;
+import de.jilocasin.kdtree.model.KdTree;
+
+public class NNSolverWorker<T extends Number & Comparable<T>> {
+	private final Thread thread;
+
+	private List<KdPoint<T>> resultPoints;
+
+	public NNSolverWorker(final KdTree<T> tree, final List<KdPoint<T>> inputPoints) {
+		this.thread = new Thread(() -> {
+			resultPoints = new ArrayList<>(inputPoints);
+
+			final NNSolver<T> solver = new NNSolver<>(tree);
+
+			for (final KdPoint<T> point : inputPoints) {
+				resultPoints.add(solver.getClosestPoint(point));
+			}
+		});
+	}
+
+	public void start() {
+		this.thread.start();
+	}
+
+	/**
+	 * Waits for this worker thread to finish, then returns the list of result
+	 * points to the caller. The index of each result point corresponds to the index
+	 * of the input points when constructing this worker.
+	 */
+	public List<KdPoint<T>> getResultPoints() {
+		try {
+			this.thread.join();
+		} catch (final InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		return resultPoints;
+	}
+}
